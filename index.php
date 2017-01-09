ff952d35e2ceaf8d908f564c5ea9f3e8b71b1043
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-09 23:32:02 +0530

    hi

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-$answer = "I don't understand. Ask me 'hi'.";
+$answer = "I don't understand.Please Ask me 'hi'.";
 if($messageText == "hi") {
     $answer = "Hello Unknown";
 }
