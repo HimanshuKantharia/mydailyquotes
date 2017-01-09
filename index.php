c5a688d4bd717710b4bf41afddcb8e98751673bc
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-10 00:06:14 +0530

    re

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if($messageText == "hi") {
 } else if ($messageText == "time") {
 	$result = file_get_contents(“http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
 
- 		if($result != ‘’) {
+ 		if($result != '') {
  			$answer = $result;
  		} else {
  			$answer = "I Didn't Get Time...";
