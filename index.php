1aa88f4fe4c06a4dc3817b14bf8965479dc6ae6c
Himanshu Kantharia <HimanshuKantharia@users.noreply.github.com>
2017-01-09 23:25:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $answer = "I don't understand. Ask me 'hi'.";
 if($messageText == "hi") {
-    $answer = "Hello";
+    $answer = "Hello Himanshu";
 }
 $response = [
     'recipient' => [ 'id' => $senderId ],
@@ -26,7 +26,7 @@ $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$acc
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
+if(!empty($messageText)){
 	curl_exec($ch);
 }
 
