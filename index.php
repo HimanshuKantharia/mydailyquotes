5283f0a34a38c03fc22f76204e960f91c3ad4c6b
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-09 23:05:43 +0530

    chng

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$acc
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
+if(!empty($messageText)){
 	curl_exec($ch);
 }
 
