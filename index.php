1f9ba628691dc3dcb0759ab6697c9b65352e82ed
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-11 00:23:04 +0530

    timeapi

--- index.php
+++ index.php
@@ -34,27 +34,18 @@ else if ($messageText == "time") {
  		}
 }
 
-public function response($senderId,$answer){
-	$response = [
-    	'recipient' => [ 'id' => $senderId ],
-    	'message' => [ 'text' => $answer ]
-	];
 
-	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+if(!empty($messageText)){
 	curl_exec($ch);
-	curl_close($ch);
-
 }
 
-//if(!empty($messageText)){
-	$res = response($senderId,$answer);
-//}
-
-
-
-
-
-
+curl_close($ch);
