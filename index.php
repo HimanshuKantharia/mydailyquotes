6e30a77c2d80fd5cb4218aa09cd731ae9f39cbbf
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-12 00:22:54 +0530

    fn

--- index.php
+++ index.php
@@ -42,12 +42,15 @@ $response = [
     'message' => [ 'text' => $answer ]
 ];
 
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($messageText)){
-	curl_exec($ch);
-}
+respond($response);
+	public function respond($response){
+		$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+		//if(!empty($messageText)){
+			curl_exec($ch);
+		//}
+		curl_close($ch);
+	}
 
-curl_close($ch);
