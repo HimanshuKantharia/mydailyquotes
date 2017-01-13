9d9d5e7f7972a16dd2fccb31bbb6821a51112da3
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-13 22:24:10 +0530

    reactivete

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$acc
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-//if(!empty($messageText)){
+if(!empty($messageText)){
 	curl_exec($ch);
-//}
+}
 curl_close($ch);
