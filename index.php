e5221ab8be366923baf165efafc69f18b25f5214
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-31 20:59:21 +0530

    Quick Replies

--- index.php
+++ index.php
@@ -52,24 +52,24 @@ $response = [
     'message' => [ 'text' => $answer ]
 ];
 
-$response1 = [
-  'recipient'=>['id'=> $senderId ],
-  'message' => [
-    "text" => "Pick a color:",
-    "quick_replies" => [
-      {
-        "content_type"=>"text",
-        "title"=>"Red",
-        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-      },
-      {
-        "content_type"=>"text",
-        "title"=>"Green",
-        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-      }
-    ]
-  ]
-];
+// $response1 = [
+//   'recipient'=>['id'=> $senderId ],
+//   'message' => [
+//     "text" => "Pick a color:",
+//     "quick_replies" => [
+//       {
+//         "content_type"=>"text",
+//         "title"=>"Red",
+//         "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+//       },
+//       {
+//         "content_type"=>"text",
+//         "title"=>"Green",
+//         "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+//       }
+//     ]
+//   ]
+// ];
 
 	
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
