6605f421c537d32c348f73e4051001b414bac697
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-01-31 20:57:48 +0530

    Quick Replies

--- index.php
+++ index.php
@@ -53,24 +53,22 @@ $response = [
 ];
 
 $response1 = [
-  "recipient":{
-    "id"=> $senderId
-  },
-  "message":{
-    "text":"Pick a color:",
-    "quick_replies":[
+  'recipient'=>['id'=> $senderId ],
+  'message' => [
+    "text" => "Pick a color:",
+    "quick_replies" => [
       {
-        "content_type":"text",
-        "title":"Red",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+        "content_type"=>"text",
+        "title"=>"Red",
+        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
       },
       {
-        "content_type":"text",
-        "title":"Green",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+        "content_type"=>"text",
+        "title"=>"Green",
+        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
       }
     ]
-  }
+  ]
 ];
 
 	
