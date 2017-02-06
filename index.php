401418f89b2782843d77101ee8ec5dc90bdb0ddf
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-02-06 20:44:35 +0530

    first query

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 
-$query = 'SELECT * FROM public."user" WHERE id='.$senderId;
+$query = 'SELECT * FROM public."user" WHERE id="'.$senderId.'"';
 $result = pg_query($conn,$query);
 if (!$result) { 
     echo "Problem with query " . $query . "<br/>"; 
