6bae51f1e1ab80477d3f4b37f492963172326c4c
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-03-05 18:50:28 +0530

    Welcome Message on Heroku

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 //$senderId = "1473360329360719"; himan
 //$senderId = "1515521005145148"; yash
 
+echo ("This is a Facebook massenger page ChatBOT: MyDailyQuotes");
 // handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
 
