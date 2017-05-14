f11e3815ac46e813193dd27d69526afb9e487e28
Himanshu kantharia <himanshu_avenger@yahoo.com>
2017-05-14 11:54:52 +0530

    adding quick replies

--- index.php
+++ index.php
@@ -128,7 +128,6 @@ else if ($messageText == "Time" || $messageText == "time") {
 	}
 }
 	
-	$qr = {'content_type' : 'text', 'title' : 'Quote', 'payload' : 'payload1'};
 
 	$response = [
     'recipient' => [ 'id' => $senderId ],
